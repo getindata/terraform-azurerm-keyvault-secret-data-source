e68459405e2cdeaa9ab37271d328c50a166ef3a7
mariusz.wojakowski <mariusz.wojakowski@truecaller.com>
2023-01-28 07:35:55 +0100

    feat: Explicitly create secret object

--- locals.tf
+++ locals.tf
@@ -15,7 +15,15 @@ locals {
     for secret in local.secrets : {
       (secret.key_vault) : {
         secrets : {
-          (secret.name) : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"]
+          (secret.name) : {
+            id : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].id
+            name : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].name
+            key_vault_id : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].key_vault_id
+            value : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].value
+            version : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].version
+            content_type : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].content_type
+            tags : data.azurerm_key_vault_secret.this["${secret.key_vault}/${secret.name}"].tags
+          }
         }
       }
     } if local.enabled
